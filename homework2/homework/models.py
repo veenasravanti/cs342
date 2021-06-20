8e94ad31353f0c64f7dec79b16681fb5b131c0ee
veenasravanti <46504278+veenasravanti@users.noreply.github.com>
2021-06-20 12:10:05 -0500

    Add files via upload

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -2,22 +2,39 @@ import torch
 
 
 class CNNClassifier(torch.nn.Module):
-    def __init__(self,layers=[],n_input_channels=3,kernel_size=3):
-        super().__init__()
-        L=[]
-        c=n_input_channels
-        for l in layers:
-          L.append(torch.nn.Conv2d(c,l,kernel_size))
-          L.append(torch.nn.ReLU())
-          c=l
-        L.append(torch.nn.Conv2d(c,1, kernel_size=1))
-        self.network=torch.nn.Sequential(*L)
+    class Block(torch.nn.Module):
+         def __init__(self, n_input, n_output, stride=1):
+                super().__init__()
+                self.net = torch.nn.Sequential(
+                  torch.nn.Conv2d(n_input, n_output, kernel_size=3, padding=1, stride=stride),
+                  torch.nn.ReLU(),
+                  torch.nn.Conv2d(n_output, n_output, kernel_size=3, padding=1),
+                  torch.nn.ReLU()
+                )
 
 
-
-    def forward(self, x):
+          def forward(self, x):
        return(self.network(x))
-
+     def __init__(self, layers=[32,64,128], n_input_channels=3):
+            super().__init__()
+            L = [torch.nn.Conv2d(n_input_channels, 32, kernel_size=7, padding=3, stride=2),
+                 torch.nn.ReLU(),
+                 torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)]
+            c = 32
+            for l in layers:
+                L.append(self.Block(c, l, stride=2))
+                c = l
+            self.network = torch.nn.Sequential(*L)
+            self.classifier = torch.nn.Linear(c, 1)
+      
+     def forward(self, x):
+            # Compute the features\n",
+            z = self.network(x)
+            # Global average pooling\n",
+            z = z.mean(dim=[2,3])
+            # Classify\n",
+            return self.classifier(z)[:,0]
+  
 
 def save_model(model):
     from torch import save
